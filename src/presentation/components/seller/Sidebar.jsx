727f6ee2dfba53d8ba7dc4b7425f1c1ae6ff2b8e
Ruui-int <136201895+Ruui-int@users.noreply.github.com>
2025-10-21 02:47:27 -0600

    bfre ultimo

--- src/presentation/components/seller/Sidebar.jsx
+++ src/presentation/components/seller/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { assets } from '@/src/assets/assets';
+import { assets } from '@/assets/assets';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
