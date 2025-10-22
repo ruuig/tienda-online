1de7a3a2b8ac649dd8267d8dea4af42d844b6300
Ruui-int <136201895+Ruui-int@users.noreply.github.com>
2025-10-21 21:55:52 -0600

    10 productos

--- src/presentation/components/HomeProducts.jsx
+++ src/presentation/components/HomeProducts.jsx
@@ -10,7 +10,7 @@ const HomeProducts = () => {
     <div className="flex flex-col items-center pt-14">
       <p className="text-2xl font-medium text-left w-full">Productos Populares</p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
-        {products.map((product, index) => <ProductCard key={index} product={product} />)}
+        {products.slice(0, 10).map((product, index) => <ProductCard key={product._id || index} product={product} />)}
       </div>
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
         Ver más
